7a68c68eb603f0e705304a29a0ab75cc64189479
Harshithal567 <Harshithalgowda24@gmail.com>
2022-05-03 16:59:49 +0530

    adding advanced java projects

--- ProblemStatement 1.2/src/com/harshitha/rectangle/Rectangle.java
+++ ProblemStatement 1.2/src/com/harshitha/rectangle/Rectangle.java
@@ -2,37 +2,34 @@ package com.harshitha.rectangle;
 
 import java.util.Scanner;
 
-public class Rectangle {
+class Rectangle {
+    int length; 
+    int breadth; 
+    int area; 
+    int perimeter;
 
-	 int length; 
-	    int breadth; 
-	    int area; 
-	    int perimeter;
+    void input() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter length of rectangle: ");
+        length = in.nextInt();
+        System.out.print("Enter breadth of rectangle: ");
+        breadth = in.nextInt();
+    }
 
-	    void input() {
-	        Scanner in = new Scanner(System.in);
-	        System.out.print("Enter length of rectangle: ");
-	        length = in.nextInt();
-	        System.out.print("Enter breadth of rectangle: ");
-	        breadth = in.nextInt();
-	    }
+    void calculate() {
+        area = length * breadth;
+        perimeter = 2 * (length + breadth);
+    }
 
-	    void calculate() {
-	        area = length * breadth;
-	        perimeter = 2 * (length + breadth);
-	    }
+    void display() {
+        System.out.println("Area of Rectangle = " + area);
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
 
-	    void display() {
-	        System.out.println("Area of Rectangle = " + area);
-	        System.out.println("Perimeter of Rectangle = " + perimeter);
-	    }
-	
-	public static void main(String[] args) {
-		
-		 Rectangle obj = new Rectangle();
-	        obj.input();
-	        obj.calculate();
-	        obj.display();
-	}
-
+    public static void main(String args[]) {
+        Rectangle obj = new Rectangle();
+        obj.input();
+        obj.calculate();
+        obj.display();
+    }
 }
